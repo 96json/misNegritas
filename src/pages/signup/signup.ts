7717fbf108f5a19c6b0a2e0e273015b4101924a0
96json <kelman.celis.iunigo.com>
2018-03-08 23:36:07 -0300

    add login temporality to signiup to firebase

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {IonicPage, NavController, ToastController} from 'ionic-angular';
 
-import { User } from '../../providers/providers';
-import { MainPage } from '../pages';
+import {User} from '../../providers/providers';
+import {MainPage} from '../pages';
+import {AngularFireAuth} from "angularfire2/auth";
 
 @IonicPage()
 @Component({
@@ -15,18 +16,19 @@ export class SignupPage {
   // If you're using the username field with or without email, make
   // sure to add it to the type
   account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
+    name: '',
+    email: '',
+    password: ''
   };
 
   // Our translated text strings
   private signupErrorString: string;
 
   constructor(public navCtrl: NavController,
-    public user: User,
-    public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+              public user: User,
+              public toastCtrl: ToastController,
+              public translateService: TranslateService,
+              public afAuth: AngularFireAuth) {
 
     this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
       this.signupErrorString = value;
@@ -35,19 +37,39 @@ export class SignupPage {
 
   doSignup() {
     // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
 
-      this.navCtrl.push(MainPage);
+    const result = () => this.afAuth.auth.createUserWithEmailAndPassword(
+      this.account.email,
+      this.account.password
+    )
+    result()
+      .then((result) => {
+        this.navCtrl.push(MainPage);
+      })
+      .catch((error) => {
+        let toast = this.toastCtrl.create({
+          message: this.signupErrorString,
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
 
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
+      })
+
+
+    /*    this.user.signup(this.account).subscribe((resp) => {
+          this.navCtrl.push(MainPage);
+        }, (err) => {
+
+          this.navCtrl.push(MainPage);
+
+          // Unable to sign up
+          let toast = this.toastCtrl.create({
+            message: this.signupErrorString,
+            duration: 3000,
+            position: 'top'
+          });
+          toast.present();
+        });*/
   }
 }
